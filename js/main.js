f0879b79ba03fd6e1edf03316ee35f273dbe0589
Belchenkov <u608110@gmail.com>
2017-02-09 05:57:22 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -2,16 +2,24 @@ $(document).ready(function ($) {
   $('#searchUser').on('keyup', function (e) {
     let username = e.target.value;
 
-    //Make request to GitHub  
+    //Make request to GitHub
+    $.ajax({
+      url: 'https://api.github.com/users/' + username,
+      data: {
+        client_id: '08a1741942752154caef',
+        client_secret: '133cb1601f1a0abba4afdcdf9e2cd284667dc6a1'
+      }
+    }).done(function (user) {
+      $('#profile').html(`
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">${user.name}</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+        </div>
+        `);
+    });
   });
 });
-
-
-
-
-
-
-// Client ID
-// 08a1741942752154caef
-// Client Secret
-// 133cb1601f1a0abba4afdcdf9e2cd284667dc6a1
