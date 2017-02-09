4c72f19649683198ee03f7de6c4e97b8417ebe78
Belchenkov <u608110@gmail.com>
2017-02-09 05:44:50 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function ($) {
+  $('#searchUser').on('keyup', function (e) {
+    let username = e.target.value;
+
+    //Make request to GitHub
+  });
+});
+
+
+
+
+
+
+// Client ID
+// 08a1741942752154caef
+// Client Secret
+// 133cb1601f1a0abba4afdcdf9e2cd284667dc6a1
